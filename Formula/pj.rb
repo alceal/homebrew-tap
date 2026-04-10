a7a74127eb14ceac08d9d97c8d3fbf0dee1999cd
alceal <alceal@gmail.com>
2026-04-10 18:30:59 +0200

    Update pj to 0.3.4

--- Formula/pj.rb
+++ Formula/pj.rb
@@ -1,8 +1,8 @@
 class Pj < Formula
   desc "Project launcher CLI with fuzzy matching"
   homepage "https://github.com/alceal/pj"
-  url "https://github.com/alceal/pj/archive/refs/tags/v0.3.3.tar.gz"
-  sha256 "167b51f7d37a6ecf25ada380706a2060529dcd4f8843f2b561ec2753bf4ba7a3"
+  url "https://github.com/alceal/pj/archive/refs/tags/v0.3.4.tar.gz"
+  sha256 "2203128006ebe24eb1576ac59f794daca0b2b1a805babde65528dadcdd3c8731"
   license "MIT"
 
   depends_on "rust" => :build
